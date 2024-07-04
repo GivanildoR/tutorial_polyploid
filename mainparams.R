a1dd2e1ebb2da9d868131c60163607eeed16ccf8
Givanildo Rodrigues Silva <167666189+GivanildoR@users.noreply.github.com>
2024-07-03 21:47:08 -0400

    Update mainparams.R

--- mainparams.R
+++ mainparams.R
@@ -9,7 +9,7 @@ output_file <- "/blue/mresende/share/Givanildo/Structure_files/RUN_STRUCTURE/mai
 #remotes::install_github("labroo2/deStructure")
 library(deStructure)
 mainparams(maxpops = NULL, burnin = 1000, numreps = 10000, infile = "structure_output.txt",
-           outfile = "my_output_file", numinds = "571", numloci = "85002",
+           outfile = "my_output_file", numinds = "571", numloci = "85002", #######CHANGE NUMINDS AND NUMLOCI ACCORDING YOUR DATA!!
            ploidy = 4, missing = -9, onerowperind = 0, label = 1,
            popdata = 0, popflag = 0, locdata = 0, phenotype = 0, extracols = 0,
            markernames = 1, recessivealleles = 0, mapdistances = 0, phased = 0,
